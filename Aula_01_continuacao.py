d85499ec05983df1e77da60a68ba39f89b42d613
EvertonSilva22 <86390461+EvertonSilva22@users.noreply.github.com>
2022-02-17 19:20:40 -0300

    primeiro desafio.
    Crie um script que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas de acordo com o valor digitado.

--- Aula_01_continuacao.py
+++ Aula_01_continuacao.py
@@ -2,3 +2,9 @@ nome= 'Everton'
 idade= '22'
 peso= 70
 print(nome, idade, peso)
+
+############################
+nome= input('Qual é o seu nome? ')
+idade= input('qual a sua idade? ')
+peso= input('Qual é o seu peso? ')
+print(nome, idade, peso)
