a21bf2322f2dba03cbf6aa5a7a5c9d0db68e2886
EvertonSilva22 <86390461+EvertonSilva22@users.noreply.github.com>
2022-02-22 18:14:38 -0300

    Desafio 02
    Utilizando string formatada.

--- Respondendo-usuario.py
+++ Respondendo-usuario.py
@@ -3,3 +3,7 @@ import string
 
 Nome= input('Qual seu nome: ')
 print('É um grande prazer te conhecer,', Nome,'!')
+
+####String Formatada####
+Nome= input('Qual é seu nome?: ')
+print('É um grande prazer te conhecer, {}!'.format(Nome))
