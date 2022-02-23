b3e6f969f15d27dccccb56dc0ae1c8122d0c320c
EvertonSilva22 <86390461+EvertonSilva22@users.noreply.github.com>
2022-02-23 19:09:26 -0300

    Aula 07
    Utilizando Format.

--- Aula_07.py
+++ Aula_07.py
@@ -6,3 +6,9 @@ N1 = int(input('Digite um valor: '))
 N2 = int(input('Digite outro valor: '))
 Soma = N1 + N2
 print('A soma vale', Soma)
+
+###Utilizando format###
+N1 = int(input('Digite um valor: '))
+N2 = int(input('Digite outro valor: '))
+Soma = N1 + N2
+print('A soma entre {} e {} vale {}'.format(N1, N2, Soma))
