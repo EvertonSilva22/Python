0a7a4456356ca336ef477b88a540c76dc93d3c72
EvertonSilva22 <86390461+EvertonSilva22@users.noreply.github.com>
2022-02-24 16:00:50 -0300

    Aula 08
    Programa criado.
    Programa que mostra a soma entre dois números.

---
+++ Aula_08.py
@@ -0,0 +1,5 @@
+###Somando dois números###
+Numero_01= int(input('Digite um valor: '))
+Numero_02= int(input('Digite outro valor: '))
+Soma= Numero_01 + Numero_02
+print('A soma entre {} e {} é igual a {}!'.format(Numero_01, Numero_02, Soma))
