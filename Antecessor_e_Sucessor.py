73b2c4e00cf02801a34bd9f4be3eec04f5da90c7
EvertonSilva22 <86390461+EvertonSilva22@users.noreply.github.com>
2022-03-10 17:53:12 -0300

    Aula 11 Python.
    Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor.

---
+++ Antecessor_e_Sucessor.py
@@ -0,0 +1,4 @@
+Numero_Digitado = int(input('Digite um numero: '))
+Antecessor = Numero_Digitado -1
+Sucessor = Numero_Digitado +1
+print('Analisando o valor {}, seu antecessor é {} e seu sucessor é {}'.format(Numero_Digitado, Antecessor, Sucessor))
