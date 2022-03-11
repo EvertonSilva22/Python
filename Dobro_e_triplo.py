31066f8079134e3272411a032503b2554b245c34
EvertonSilva22 <86390461+EvertonSilva22@users.noreply.github.com>
2022-03-11 17:46:50 -0300

    Exercício Dobro Triplo e Raiz.
    Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.

---
+++ Dobro_e_triplo.py
@@ -0,0 +1,9 @@
+Numero = int(input('Digite um número: '))
+
+Dobro = Numero *2
+Triplo = Numero *3
+Raiz_Quadrada = Numero **(1/2)
+
+print('O Dobro de {} vale {}'.format(Numero, Dobro))
+print('O triplo de {} vale {}'.format(Numero, Triplo))
+print('A raiz quadrada de {} é {:.2f}'.format(Numero, Raiz_Quadrada))
