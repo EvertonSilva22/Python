304909466d75667378e0f49eae8d5b1095184fc2
EvertonSilva22 <86390461+EvertonSilva22@users.noreply.github.com>
2022-03-12 23:38:01 -0300

    Exercício_07 média Aritimética.
    Programa criado.

---
+++ media_aritimetica.py
@@ -0,0 +1,4 @@
+Nota_01 = int( input('Primeira nota do aluno: '))
+Nota_02 = int(input('Segunda nota do aluno: '))
+media = (Nota_01 + Nota_02) /2
+print('A média entre {:.2f} e {:.2f} é igual a {:.2f}'.format(Nota_01, Nota_02, media))
